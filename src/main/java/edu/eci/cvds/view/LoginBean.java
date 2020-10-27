65576471695de67b609444e3530ec956bb5a8100
Andres Calderon <andres.calderon-o@escuelaing.edu.co>
2020-10-27 10:01:16 -0500

    Login Bean Test

---
+++ src/main/java/edu/eci/cvds/view/LoginBean.java
@@ -0,0 +1,17 @@
+package edu.eci.cvds.view;
+
+import edu.eci.cvds.entities.Usuario;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.util.List;
+
+@ManagedBean(name = "loginBean")
+@SessionScoped
+@ApplicationScoped
+
+public class LoginBean {
+}
