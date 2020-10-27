bba66442c2ae944dec90092b00576d70cbcd9ad0
Andres Calderon <andres.calderon-o@escuelaing.edu.co>
2020-10-27 10:15:30 -0500

    Excepciones Class

---
+++ src/main/java/edu/eci/cvds/exceptions/HistorialEquiposException.java
@@ -0,0 +1,9 @@
+package edu.eci.cvds.exceptions;
+
+public class HistorialEquiposException extends Exception{
+
+    public HistorialEquiposException(String message) {
+        super(message);
+    }
+
+}
