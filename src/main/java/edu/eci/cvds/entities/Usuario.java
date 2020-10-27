68731484e556c689840f2268e0d920caaf6fc0a0
Andres Calderon <andres.calderon-o@escuelaing.edu.co>
2020-10-27 09:38:18 -0500

    Entidades

---
+++ src/main/java/edu/eci/cvds/entities/Usuario.java
@@ -0,0 +1,14 @@
+package edu.eci.cvds.entities;
+
+public class Usuario {
+    private int carnet;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String userName;
+    private String password;
+    private String estado;
+
+    public Usuario(){}
+
+}
