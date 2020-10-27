042a62805023e4b5a5457e34e22536273986fbd9
Andres Calderon <andres.calderon-o@escuelaing.edu.co>
2020-10-27 09:58:26 -0500

    Java Class Test

---
+++ src/test/java/edu/eci/cvds/TestHistorialDeEquipos.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds;
+
+import com.google.inject.Inject;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TestHistorialDeEquipos {
+
+    @Inject
+    private SqlSession sqlSession;
+
+}
