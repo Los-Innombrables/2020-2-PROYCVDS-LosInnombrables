946c86ac8d04cccce8e1040bea16cec6c159d8e9
Daniel R <daniel.rincon-m@mail.escuelaing.edu.co>
2020-11-16 17:16:07 -0500

    Botón de barra lateral funcionando y filtro de admin

--- src/main/java/edu/eci/cvds/security/AdministrationFilter.java
+++ src/main/java/edu/eci/cvds/security/AdministrationFilter.java
@@ -13,36 +13,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/faces/dashboard/*")
-public class DashboardFilter implements Filter {
+import edu.eci.cvds.entities.Usuario;
+
+@WebFilter("/faces/dashboard/administrar/*")
+public class AdministrationFilter implements Filter {
 
     public static final String LOGIN_PAGE = "/faces/login.xhtml";
+    private static final String MAIN_PAGE = "/faces/dashboard/main.xhtml";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        // TODO Auto-generated method stub
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         HttpSession session = req.getSession(false);
         // boolean ajaxRequest = "partial/ajax".equals(req.getHeader("Faces-Request"));
+        Usuario user = (Usuario) session.getAttribute("user");
 
-        boolean loggedIn = (session != null) && (session.getAttribute("user") != null);
+        boolean loggedIn = (session != null) && (user != null);
 
         if (loggedIn) {
-            // System.out.println("dashboard Filter meh");
-            chain.doFilter(request, response);
+            if (user.getRol() == 1) {
+                chain.doFilter(request, response);
+            } else {
+                // System.out.println("Admin 1 Filter Working");
+                res.sendRedirect(MAIN_PAGE);
+            }
         } else {
-            // System.out.println("dashboard Filter Working");
+            // System.out.println("Admin Filter Working");
             res.sendRedirect(LOGIN_PAGE);
         }
     }
 
     @Override
     public void destroy() {
+        // TODO Auto-generated method stub
 
     }
 
